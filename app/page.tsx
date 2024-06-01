52f9284631db15f75e18accbe8dad76b2d2a9fc5
Kyle <kyle.tz.cui@gmail.com>
2024-06-01 13:45:50 -0700

    add description to home

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
       <div className="p-10">
         <h1 className="pb-2 font-bold text-2xl">Chat With Any Website</h1>
         <h3 className="pb-5 text-slate-400 italic">@kile_sway</h3>
+
+        <p>Ask ChatGPT about any website, just paste the website URL to the end of this website.</p>
+        <p>Ex. to chat with https://example.com navigate to https://chatwithanywebsite.vercel.app/https://example.com</p>
+        <p>Only works with http and https websites, and websites that don't block bots!</p>
       </div>
     </div>
   )
